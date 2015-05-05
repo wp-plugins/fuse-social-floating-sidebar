afae1c3aeefdc9455f7d7401bfa75105ee9027c9
daniyalahmedk <daniyalahmedk@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-05-05 19:38:52 +0000

    tag release plugin
    git-svn-id: https://plugins.svn.wordpress.org/fuse-social-floating-sidebar/tags/1.0@1154105 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ fuse_social_sidebar.php
@@ -0,0 +1,40 @@
+<?php
+/*
+Plugin Name: Fuse Social Floating Sidebar
+Plugin URI: http://photontechs.com/fuse-social-sidebar
+Description: This Fuse Social Floating Sidebar plugin allow you to put social icons which can be link with your social media profiles.
+Version: 1.0
+Author: Daniyal Ahmed
+Author URI: http://www.photontechs.com
+License: GNU General Public License v3.0
+License URI: http://www.opensource.org/licenses/gpl-license.php
+NOTE: This plugin is released under the GPLv2 license. The icons used in this plugin are the property
+of their respective owners, and do not, necessarily, inherit the GPLv2 license.
+*/
+// Adding Admin Menu
+require_once('inc/fuse_social_sidebar_admin.php');
+// Getting Values from options
+$options = get_option('fuse_social_options');
+// Creating Icons
+require_once('inc/fuse_social_sidebar_func.php');
+// Getting Style for awesome icons
+require_once('inc/fuse_social_sidebar_scripts.php');
+// Add settings link on plugin page
+function fuse_social_settings_link($links) { 
+  $settings_link = '<a href="options-general.php?page=fuse-social-sidebar">Settings</a>'; 
+  array_unshift($links, $settings_link); 
+  return $links; 
+}
+$plugin = plugin_basename(__FILE__); 
+add_filter("plugin_action_links_$plugin", 'fuse_social_settings_link' );
+add_action('wp_footer','fuse_social_sidebar',100);
+
+
+		function fuse_social_sidebar()
+		{
+				$makeawesome_icons = new Making_Fuse_Icons;
+				// Getting Icons for Shortcode
+				$makeawesome_icons->Create_Awesome_Icons();
+		}
+
+?>
